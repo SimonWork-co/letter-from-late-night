32afb3ea03af5198210f7d8b3411bd2cf6e9ab1d
sangmokchoi <cefho17@hanmail.net>
2023-04-03 10:38:02 +0900

    초기 진입 시 안내 페이지 생성 및 수정중

---
+++ Simonwork2/Model/SecondView.swift
@@ -0,0 +1,26 @@
+//
+//  CustomizedCell.swift
+//  Simonwork2
+//
+//  Created by Sangmok Choi on 2023/03/10.
+//
+
+import UIKit
+
+class SecondView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        // xibSetup() // 하면 storyboard에서 실시간(컴파일타임)에 inspector창에서 변경해도 확인 불가
+    }
+    
+
+}
